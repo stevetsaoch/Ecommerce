4d847da14171d85d6bad28229b23392c5b0be03a
stevetsaoch <stevetsaoch@gmail.com>
2022-02-10 16:33:39 +0800

    update some names'

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('store/', include('store.urls', namespace='store')),
+    path('', include('store.urls', namespace='store')),
 ]
 
 if settings.DEBUG:
