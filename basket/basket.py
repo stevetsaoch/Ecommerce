904ef78a4c25d652025ed84c360204c721123bb0
stevetsaoch <stevetsaoch@gmail.com>
2022-02-15 21:54:06 +0800

    shopping basket finished

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,6 @@
+from itertools import product
+from store.models import Product
+from decimal import *
 
 
 class Basket():
@@ -10,5 +13,60 @@ class Basket():
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number': 123321}
+            basket = self.session['skey'] = {}
         self.basket = basket
+
+    def add(self, product, qty):
+        product_id = product.id
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        else:
+            self.basket[product_id] = {
+                'price': str(product.price),
+                'qty': int(qty),
+            }
+        self.session.modified = True
+
+    def delete(self, product):
+        """
+        Delete item from session data
+        """
+        product_id = str(product)
+        if product_id in self.basket:
+            del self.basket[product_id]
+
+        self.session.modified = True
+
+    def update(self, product, qty):
+        """
+        Update item qty in session data
+        """
+        product_id = str(product)
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+
+        self.session.modified = True
+
+    def __len__(self):
+        """
+        get basket data and count the qty items.
+        """
+        return sum(item['qty'] for item in self.basket.values())
+
+    def __iter__(self):
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
