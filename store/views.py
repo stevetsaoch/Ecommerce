18be66ba48020e6d9a0a2b1e84dc9cc70e525a25
stevetsaoch <stevetsaoch@gmail.com>
2022-03-01 16:18:26 +0800

    refactor store template, payment app.

--- store/views.py
+++ store/views.py
@@ -5,13 +5,13 @@ from .models import Category, Product
 
 def product_all(request):
     products = Product.products.all()
-    return render(request, 'store/home.html', context={
+    return render(request, 'store/index.html', context={
         'products': products})
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/single.html', context={
+    return render(request, 'store/single.html', context={
         'product': product}
     )
 
@@ -19,7 +19,7 @@ def product_detail(request, slug):
 def category_list(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
-    return render(request, 'store/products/category.html', context={
+    return render(request, 'store/category.html', context={
         'category': category,
         'products': products
     })
