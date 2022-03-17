adecba2d0bad9da93681353c82c876aac6696d9a
stevetsaoch <stevetsaoch@gmail.com>
2022-03-17 14:17:45 +0800

    basic pagination

--- store/views.py
+++ store/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 
 def product_all(request):
     products = Product.products.all()
-    return render(request, "store/index.html", context={"products": products})
+    page_num = request.GET.get("page", 1)
+    paginator = Paginator(products, 10)
+
+    try:
+        page_object = paginator.page(page_num)
+    except PageNotAnInteger:
+        page_object = paginator.page(1)
+    except EmptyPage:
+        page_object = paginator.page(paginator.num_pages)
+
+    return render(request, "store/index.html", context={"product_page": page_object})
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
     qty = product.inventory
-    _range = range(0,1)
+    _range = range(0, 1)
     if qty > 0 and qty < 5:
         _range = range(1, product.inventory + 1)
     elif qty > 5:
