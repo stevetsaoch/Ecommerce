f492000f0b88ff860208ba589e0a9197da248407
stevetsaoch <stevetsaoch@gmail.com>
2022-04-08 13:33:57 +0800

    remove unused function in order/forms and views; update template

--- orders/forms.py
+++ orders/forms.py
@@ -1,16 +1,9 @@
 from django import forms
 from account.models import UserBase
-
 from orders.models import Order
 
 
 class OrderIssue(forms.Form):
-    # def __init__(self, order: Order, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.order = order
-    #     self.user = UserBase.objects.get(id=order.user_id)
-    #     self.fields["email"].initial = self.user.email
-    #     self.fields["orderid"].initial = self.order.id
 
     email = forms.EmailField(
         label="Your email (can not be changed)",
@@ -27,7 +20,7 @@ class OrderIssue(forms.Form):
     )
 
     orderid = forms.CharField(
-        label="Your order id.",
+        label="Your order id",
         max_length=255,
         widget=forms.TextInput(attrs={"class": "form-control mb-3", "placeholder": "orderid", "id": "form-orderid", "readonly": "readonly"}),
     )
